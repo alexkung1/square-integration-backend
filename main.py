ec46afc5d61acbd13b5cda2c790f6cc058737edd
Alexander Kung <akung@secretmission.io>
2019-06-13 09:29:59 -0400

    changes from june 12

--- main.py
+++ main.py
@@ -8,8 +8,20 @@ from squareconnect.apis.o_auth_api import OAuthApi
 from squareconnect.models.obtain_token_request import ObtainTokenRequest
 import json
 import urllib
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+db = SQLAlchemy(app)
+
+### NGROK
+base_url = "https://278417fc.ngrok.io/"
+
+
+#################################
+#######   SQUARE AUTH  ##########
+#################################
 
 location_id = "VKA3ZBDKC21TQ"
 access_token = "EAAAECrvvArmS9YlaFw9q9UrDBvGTRokwReytSbjjjKVEIF5GaEI3417iLSQtDBq"
@@ -24,9 +36,74 @@ _SQ_AUTHZ_URL = "/oauth2/authorize"
 tokens = {}
 permissions = ["PAYMENTS_READ", "PAYMENTS_WRITE"]
 
+#################################
+#################################
+#################################
+
+######################################
+#######   LOGIN MANAGEMENT  ##########
+######################################
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    results = User.query.filter_by(id=user_id)
+    if results.all():
+        return results.first()
+    else:
+        return None
+
+
+######################################
+######################################
+######################################
+
+
+############################
+#######   MODELS  ##########
+############################
+
+
+class Token(db.Model):
+    __tablename__ = "Token"
+
+    id = db.Column(db.Integer, primary_key=True)
+    auth_token = db.Column(db.String)
+
+    def __repr__(self):
+        return "<Token(auth_token='%s')>" % (self.auth_token,)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    is_authenticated = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=False)
+    is_anonymous = db.Column(db.Boolean, default=False)
+
+    def get_id(self):
+        return str(self.id).encode("utf-8").decode("utf-8")
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+
+############################
+############################
+############################
+
 
 def getObtainTokenRequest():
     r = requests.get("https://api.github.com/user", auth=("user", "pass"))
+
+
+#################################
+########## ENDPOINTS ############
+#################################
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -48,11 +125,48 @@ def home():
     return render_template("main.html", url=link2, permissions=permissions)
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html", tokens=tokens)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/authenticate_user", methods=["GET"])
+def authenticate_user():
+    
+
+@app.route("/register")
+def register():
+    return render_template("register.html", url=base_url+"register_backend", home=base_url)
+
+@app.route("/register_backend")
+def register_backend():
+    username = request.data.get("username")
+    email = request.data.get("email")
+    try:
+        user = User(username=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return "Registration successful!"
+    except:
+        return "Registration failed"
+
+
+
+
 @app.route("/request_token", methods=["POST"])
 def request_token():
     data = json.loads(request.data)
     print(data)
     return "Hello, Salvador"
+
+
+@app.route("/show_tokens")
+def show_tokens():
+    tokens = Token.query.all()
+    return render_template("tokens.html", tokens=tokens)
 
 
 @app.route("/s_location", methods=["GET", "POST"])
@@ -95,9 +209,15 @@ def callback():
 
             # Here, instead of printing the access token, your application server should store it securely
             # and use it in subsequent requests to the Connect API on behalf of the merchant.
-            print("Access token: " + response.access_token)
-            tokens["access_token"] = response.access_token
-            return "Authorization succeeded!"
+            token_str = response.access_token
+            # print("Access token: " + token_str)
+            # tokens["access_token"] = token_str
+            token = Token(auth_token=token_str)
+            db.session.add(token)
+            db.session.commit()
+
+            # return "Authorization succeeded!"
+            return render_template("auth_success.html")
 
         # The response from the Obtain Token endpoint did not include an access token. Something went wrong.
         else:
